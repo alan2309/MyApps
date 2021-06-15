a3ba96d49dbc949e0e851642118ef2ad6511bc8b
Alankrit Arya <alankritarya15@gmail.com>
2021-06-15 14:52:59 +0530

    Delete destini-challenge directory

--- destini-challenge/lib/story.dart
+++
@@ -1,9 +0,0 @@
-class Story {
-  String storyTitle, choice1, choice2;
-  Story({String storyTitle, String choice1, String choice2}) {
-    this.storyTitle = storyTitle;
-    this.choice1 = choice1;
-    this.choice2 = choice2;
-  }
-}
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
