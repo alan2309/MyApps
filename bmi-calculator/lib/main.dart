7f7cf4a0a89a11106a8b9b93126f1386e95c4504
Alankrit Arya <alankritarya15@gmail.com>
2021-06-15 14:58:26 +0530

    Update main.dart

--- bmi-calculator/lib/main.dart
+++ bmi-calculator/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator/screens/input_page.dart';
 
+
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
